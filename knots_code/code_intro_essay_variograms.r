196f638c03d4b25aa4d5d3b5e45725346c07266f
Pratik Gupte <pratikgupte16@gmail.com>
2019-01-21 15:45:43 +0100

    add intro essay files

--- knots_code/code_intro_essay_variograms.r
+++ knots_code/code_intro_essay_variograms.r
@@ -4,10 +4,10 @@ library(tidyverse);library(NLMR);library(raster)
 
 #### how does sill vary with mag var ####
 
-landscape_params = rep(data_frame(acr = (1:10), mag = (1:10)*5) %>% expand(acr, mag) %>% 
-  plyr::dlply(c("acr","mag")))
+landscape_params = rep(data_frame(acr = (c(2, 20, 80))) %>% 
+  plyr::dlply(c("acr")))
 
-landscapes = map(landscape_params, function(x) {nlm_gaussianfield(1e2, 1e2, autocorr_range = x$acr, mag_var = x$mag, rescale = F)})
+landscapes = map(landscape_params, function(x) {nlm_gaussianfield(1e2, 1e2, autocorr_range = x$acr, rescale = T)})
 
 #'raster to df function
 raster_to_df = function(x){
@@ -18,32 +18,45 @@ landscape_df = map(landscapes, raster_to_df)
 
 landscape_df = landscape_df %>% bind_rows()
 
-landscape_params = landscape_params %>% bind_rows() %>% mutate(id = paste(stringi::stri_pad(acr, 3, pad = 0), stringi::stri_pad(mag, 2, pad = 0), sep = "_"))
+landscape_params = landscape_params %>% bind_rows() %>% mutate(id = paste(stringi::stri_pad(acr, 3, pad = 0)))
 
 landscape_df = landscape_df %>% 
   mutate(id = rep(landscape_params$id, each = 1e4)) %>% 
-  mutate(acr = substr(id, 1, 3), mag = substr(id, 5, 6))
+  mutate(acr = as.numeric(substr(id, 1, 3)))
 
 library(RColorBrewer)
 
-ggplot(landscape_df)+
+fig2a = ggplot(landscape_df)+
   geom_raster(aes(x = col, y = row, fill = plyr::round_any(val, 0.1)))+
-  facet_grid(acr~mag)+
-  scale_fill_gradientn(colours = plasma(20))+
-  theme_classic()+theme(legend.position = "none")
+  facet_wrap(~acr)+
+  scale_fill_gradientn(colours = brewer.pal(7, "RdBu"))+
+  theme_pub()+theme(legend.position = "none")+
+  labs(list(x = NULL, y = NULL, title = "(a)"))
 
 
 library(gstat)
-for(i in 1:length(landscape_df)){
-  coordinates(landscape_df[[i]]) = ~ col + row
-  landscape_df[[i]] = variogram(val ~ col+row, data = landscape_df[[i]])
+for(i in 1:length(landscapes)){
+  landscapes[[i]] = raster_to_df(landscapes[[i]])
+  coordinates(landscapes[[i]]) = ~ col + row
+  landscapes[[i]] = variogram(val ~ col+row, data = landscapes[[i]])
   
 }
 
-landscape_df = landscape_df %>% map(function(x){x %>% as.data.frame() %>% dplyr::select(dist, gamma)})
+landscape_vargrams = landscapes %>% map(function(x){x %>% as.data.frame() %>% dplyr::select(dist, gamma)})
 
-landscape_df = landscape_df %>% bind_rows() %>% mutate(acr = rep((1:10)*20, each = 150), mag = rep(rep((1:10)*5, each = 15), 10))
+source("ggplot.pub_geese.r")
 
-ggplot(landscape_df)+
-  geom_point(aes(dist, gamma))+
-  facet_grid(acr ~ mag)
+landscape_vargrams = landscape_vargrams %>% bind_rows() %>% mutate(acr = rep(c(2,20,80), each = 15))
+
+fig2b = 
+  ggplot(data = landscape_vargrams, aes(dist, (1/gamma)/1e3))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~acr)+
+  theme_pub()+
+  geom_hline(yintercept = 0.1, col = 2)+
+  labs(list(y = "Autocorrelation", x = "Distance (grid cells)", title = "(b)"))
+
+library(gridExtra)
+
+grid.arrange(fig2a, fig2b, nrow = 2)  
