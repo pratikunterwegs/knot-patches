9c4fdf5e92ab1f5a04f1b3ea39cf3c3463343693
Pratik Gupte <pratikgupte16@gmail.com>
2019-02-05 16:27:23 +0100

    add cover art

--- knots_code/code_intro_essay_cover.R
+++ knots_code/code_intro_essay_cover.R
@@ -10,20 +10,44 @@ data = data2018.raw %>%
   keep(function(x)length(x)>0) %>% 
   map(bind_rows)
 #'keep only data[1] now
-#data = data[[6]]
+#data = data[[2]]
 
 #'convert to x,y,t
-data = data %>% map(function(x) select(x, time=TIME, x=X, y=Y, covxy = COVXY))
+data = data %>% 
+  map(function(x) select(x, time=TIME, x=X, y=Y, covxy = COVXY, towers = NBS)) %>% 
+  map(function(x) mutate(x, time = plyr::round_any(time/1000, 10))) %>% 
+  map(function(x) x %>% filter(towers > 3) %>% group_by(time) %>% summarise_all(mean))
 
 library(sf)
 #'make linestring df object
 dataline = map(data, function(x){
   st_linestring(x = cbind(x$x, x$y), dim = "XY")})
 
-dataline = map(dataline, function(x){st_sfc(x, crs = 32630)})
-#'plot all 10
-par(mfrow = c(2,5))
-for(i in 1:10) plot(dataline[[i]])
+#'make points
+datapoints = map(data, function(x){
+  st_multipoint(x = cbind(x$x, x$y), dim = "XY")
+})
 
-#'write 2nd agent to shapefile
+#'assign crs and make sfc
+dataline = map(dataline, function(x){st_sfc(x, crs = 32631)})
+datapoints = map(datapoints, function(x){st_sfc(x, crs= 32631)})
+
+#'plot all 10
+x11(); par(mfrow = c(2,5)); for(i in 1:10) plot(dataline[[i]])
+
+x11(); plot(datapoints[[2]], cex = 0.1)
+
+#'write knot 471 to shapefile
 write_sf(dataline[[2]], dsn = "intro_essay_track", layer = "knot_track_471", driver = "ESRI Shapefile")
+#'write points
+write_sf(datapoints[[2]], dsn = "intro_essay_points", layer = "knot_points_471", driver = "ESRI Shapefile")
+
+#'get neutral landscape
+library(NLMR);library(raster)
+landscape = nlm_gaussianfield(ncol = 148, nrow = 105)
+library(RColorBrewer)
+pal = colorRampPalette(brewer.pal(9, "Greys")[c(1:5)])(20)
+
+png(filename = "intro_essay_cover_part01.png", width = 148, height = 105, units = "mm", res = 300)
+par(mar = c(0,0,0,0)); image(landscape, col = pal, axes = F, box =F, bty = "n", legend=F)
+dev.off()
