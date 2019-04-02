be1dc2208c8d718443b42d11731e1d7c6b12eadb
pratik <pratikgupte16@gmail.com>
2019-04-02 12:13:19 +0200

    finetune FPT code for recurse

--- knots_code/codeMoveMetrics/code_FPT.R
+++ knots_code/codeMoveMetrics/code_FPT.R
@@ -8,24 +8,41 @@ library(tidyverse); library(readr)
 #'read in the data
 data = read_csv("../data2018/data2018posWithTides.csv")
 
-#'subset for good forage data
-goodForageData = read_csv("../data2018/goodForageData.csv")
+#'subset for good data
+#'
+#'A NOTE ON GOOD DATA
+#'GOOD DATA ARE:
+#' PER-ID PER-TIDE DATA WHERE:
+#'  > 0.33 OF THE DURATION
+#'HAS BEEN COVERED, IE, 139 MINUTES BETWEEN START AND END OF TRACKING
+#'AND
+#'AND
+#'WHERE:
+#' > 0.33 OF THE EXPECTED FIXES ARE PRESENT.
+#' 
+#' EACH ID-TIDE COMBINATION THUS HAS AT LEAST 831 OBSERVATIONS
+#'
+goodData = read_csv("../data2018/dataSummary2018.csv") %>% 
+  mutate(id.tide = paste(id,stringr::str_pad(tidalCycle, 3, pad = "0"),sep = ".")) %>% 
+  filter(propDur >= 0.33, propFixes >= 0.33)
 
-data = filter(data, id %in% goodForageData$id, tidalCycle %in%
-                goodForageData$tidalCycle) %>%
-  filter(between(timeToHiTide, 3*60, 10*60))
+#'write good data to file
+write_csv(goodData, path = "../data2018/goodData2018.csv")
+
+data = mutate(data, 
+              id.tide = paste(id,stringr::str_pad(tidalCycle, 3, pad = "0"),sep = ".")) %>% 
+  filter(id.tide %in% goodData$id.tide)
 
 #### prepare for recursion analysis ####
 #'make list of id - tidalCycle combination
-data = plyr::dlply(data, c("id", "tidalCycle"))
+data = plyr::dlply(data, "id.tide")
 #'remove dataframes with less than 33% points, ~139
-data = purrr::keep(data, function(x) nrow(x) >= 150)
+#data = purrr::keep(data, function(x) nrow(x) >= 150)
 
 #'prepare for recurse by exporting to file
 for(i in 1:length(data)){
-  write_csv(data[[i]], path = paste("../data2018/dataRecurse/id",
-            unique(data[[i]]$id), "tide",
-            unique(data[[i]]$tidalCycle, ".csv")))
+  write_csv(data[[i]], path = paste("../data2018/dataRecurse/id.tide", 
+                                    unique(data[[i]]$id.tide),".csv", sep = ""))
 }
 
 #'prepare files list to read in data
@@ -33,11 +50,18 @@ recurseFiles = list.files(path = "../data2018/dataRecurse/", full.names = T)
 
 library(recurse)
 
+#### run recurse ####
+
 #'in a for loop, read files, make recurse, write to file, remove data
 for(i in 1:length(recurseFiles)){
-  x = read_csv(recurseFiles[i])
+  x = as.data.frame(read_csv(recurseFiles[i],
+               col_types = list("n", "T", "n", "n",
+                             "?", "?", "?", "?",
+                             "?", "?", "?", "?",
+                             "c")))
 
-  tide = unique(x$tidalCycle); id = unique(x$id)
+  id.tide = as.character(unique(x$id.tide))
+  #id = unique(x$id); tide = unique(x$tidalCycle)
 
   x = x[,c("x", "y", "time", "id")]
 
@@ -48,57 +72,46 @@ for(i in 1:length(recurseFiles)){
 
   #'get FPT as first residence time
   xFpt = xRecurse[["revisitStats"]] %>%
-          group_by(coordIdX, visitIdX) %>%
-          summarise(fpt = first(residenceTime))
+          group_by(coordIdx) %>%
+          summarise(fpt = first(timeInside))
 
   #'make tibble
-  xRecurseData = tibble(id = id, tide = tide,
-            residenceTime = xRecurse[["residenceTIme"]],
+  xRecurseData = tibble(id.tide = id.tide,
+            residenceTime = xRecurse[["residenceTime"]],
             revisits = xRecurse[["revisits"]],
             fpt = xFpt$fpt)
 
   #'clear memory
-  rm(x, tide, id, xRecurse, xFpt); gc()
+  rm(x, xRecurse, xFpt); gc()
 
   #'write recurse to file
-  write_csv(xRecurseData, path = paste("../data2018/dataForageRecurse/id",
-            id, "tide",
-            tide, ".csv"))
+  write_csv(xRecurseData, path = paste("../data2018/dataRecurseStats/", 
+            id.tide, ".csv"))
 
   #'remove data
-  rm(xRecurseData); gc()
+  rm(xRecurseData, id.tide); gc()
 }
 
 #### read data back in and get FPT ####
 #'list files
-recurseFiles = list.files(path = "../data2018/dataForageRecurse",
+recurseFiles = list.files(path = "../data2018/dataRecurseStats",
                           full.names = T)
 
 #'read the files in
-recurseData = lapply(recurseFiles, read_csv)
+recurseData = lapply(recurseFiles, function(x){
+  read_csv(x, col_types = list("_", "n", "n", "n"))
+})
 
-#'bind to existing data
+#'bind to existing data, then bind rows
 data = map2(data, recurseData, cbind)
+data = bind_rows(data)
 
-#### potential LTRAJ method ####
+#'check to see if each id.tide combo has 831 obs
+count(data, id.tide) %>% count(n > 830)
+#'9 do not fit this criterion
 
-#'convert to ltraj
-library(adehabitatLT)
+#'remove recurse data
+rm(recurseData)
 
-dataLtraj = lapply(data, function(x){
-  as.ltraj(xy = x[,c("x","y")], date = x$time, id = x$id,
-           proj4string = CRS("+proj=utm +zone=31 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
-})
-
-#'get fpt
-dataFpt = lapply(dataLtraj, function(x){
-  fpt(x, radii = 100, units = "seconds")
-})
-
-#'make tibble of nested data
-dataFpt = lapply(dataFpt, function(x) x[[1]])
-dataFptNest = tibble(id.tide = names(dataFpt), fpt = dataFpt) %>%
-  unnest()
-
-#'write to file
-write_csv(dataFptNest, path = "../data2018/data2018FptForage.csv")
+#### write good recurse data to file ####
+write_csv(data, path = "../data2018/data2018WithRecurse.csv")
