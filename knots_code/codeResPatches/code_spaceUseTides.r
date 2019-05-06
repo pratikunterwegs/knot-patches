c5f48c4ec5c0b3c3fbb9f6afaeb252da09de2168
pratikunterwegs <pratikgupte16@gmail.com>
2019-05-06 15:09:06 +0200

    convert segclust2d to k-means clustering

--- knots_code/codeResPatches/code_spaceUseTides.r
+++ knots_code/codeResPatches/code_spaceUseTides.r
@@ -1,6 +1,6 @@
-#### code to segment on residence ####
+#### code to kmeans cluster points ####
 
-# this code is experimental and doesn't full work
+# this code is experimental and doesn't fully work
 # without errors
 
 #### load libs ####
@@ -8,48 +8,82 @@ library(tidyverse); library(readr)
 rm(list = ls()); gc()
 
 # load position data with residence times
-data = read.csv("../data2018/data2018WithRecurse.csv") %>% 
-  select(id.tide, timeNum, x, y, residenceTime)
+data = read_csv("../data2018/data2018WithRecurse.csv") %>% 
+  select(id, tidalCycle, x, y, timeNum, residenceTime)
 
 # split by id and tide
-data = plyr::dlply(data, "id.tide")
+dataNest = group_by(data, id, tidalCycle) %>% 
+  nest()
 
-# run segclust2d on residence time
-library(segclust2d)
+# remove data
+rm(data); gc()
 
-# write segmentation function for data
-# choose lmin = 5
-# choose Kmax = auto
-# seg.var = "residenceTime"
-# no subsampling
+# run Kmeans residence time
+approxTimeBwSegs = 60*30 # in seconds, 10 mins
+
+# write kmeans function for data
+# choose centres  = duration in minutes / 10
 funcSegment = function(x){
+  # kmeans reqs a matrix or coercable object such as a df
   assertthat::assert_that(is.data.frame(x), msg = "x is not a df!")
-  x1 = segmentation(x, lmin = 15, seg.var = "residenceTime")
-  return(x1)
-}
-
-# function to add data to position data
-funcAugment = function(y){
-  assertthat::assert_that(class(y) == "segmentation",
-                          msg = "input is not a segclust2d output")
-  
-  y1 = augment(y)[c("state_ordered")]
-  return(y1)
+  nAssumedPatches = floor((max(x$timeNum) - min(x$timeNum))/approxTimeBwSegs)
+  x1 = kmeans(x[,c("x", "y", "timeNum")], centers = nAssumedPatches)
+  return(x1[["cluster"]])
 }
 
 # run across the list of dfs
-for (i in 723:length(data)) {
+dataNest = mutate(dataNest, 
+                  data = map(data, function(df){
+                    mutate(df, segment = funcSegment(df))
+                  }))
+
+#### plot segments ####
+# random 25 id~tide combos
+dataSubset = sample_n(dataNest, 25) %>% 
+  unnest() %>% 
+  mutate(bird = id) %>% 
+  group_by(bird, tidalCycle) %>% nest()
   
-  # run the function within a function that catches errors
-  # and ignores them!
-  # if a knot-id 
-  tryCatch({
-  segmentedDf = funcSegment(data[[i]])
-  augmentedDf = funcAugment(segmentedDf)
-  data[[i]]$segment = augmentedDf$state_ordered
-  
-  write_csv(data[[i]], 
-            path = paste("../data2018/segmentation/", data[[i]]$id.tide[1], "segmented.csv", sep = ""))
-  
-  gc()}, error = function(e){})
+
+# read in griend
+library(sf)
+griend = st_read("../griend_polygon/griend_polygon.shp")
+
+# make list plot
+listPlots = map(dataSubset$data, function(z){
+  ggplot(griend)+
+    geom_sf(col = 2)+
+    geom_point(data = z, 
+               aes(x, y, col = factor(segment)), size = 0.5)+
+    geom_path(data = z, aes(x, y, col = factor(segment)), size = 0.1)+
+    #scale_fill_manual(values = pals::kovesi.rainbow(length(unique(z$segment))))+
+    scale_colour_manual(values = pals::kovesi.rainbow(length(unique(z$segment))))+
+    #coord_sf(datum = NA)+
+    theme_bw()
+})
+
+# export plots
+# export to single pdf
+pdf(file = "../figs/figResPatchesMap.pdf", width = 297/25.4, height = 210/25.4)
+for (i in 1:25) {
+  print(listPlots[[i]]+
+          labs(title = paste(dataSubset$bird[i],
+                                  dataSubset$tidalCycle[i], sep = " ")))
 }
+dev.off()
+
+# residence vs time plot
+ggplot(dataSubset %>% unnest())+
+  # geom_ribbon(aes(x = 1:length(residenceTime), ymin = min(residenceTime),
+  #                 ymax = max(residenceTime), fill = factor(segment)),
+  #             alpha = 0.2)+
+  
+  #  geom_line(aes(x = 1:length(residenceTime), y = residenceTime), size = 0.1)+
+  geom_point(aes(x = 1:length(residenceTime), y = residenceTime, col = factor(segment)), size = 0.1)+
+  geom_line(aes(x = 1:length(residenceTime), y = residenceTime, col = factor(segment)), size = 0.1)+
+  scale_colour_manual(values = pals::kovesi.rainbow(26))+
+  facet_wrap(~bird+tidalCycle, scales = "free_x")+
+  theme_bw()+theme(legend.position = "none")
+
+ggsave("../figs/figResPatchesResTimeVsTime.pdf", device = pdf(), 
+       width = 350, height = 350, units = "mm"); dev.off()
