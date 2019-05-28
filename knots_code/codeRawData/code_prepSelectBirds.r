9b18515a06bc12e0009dbd5e96d738e382e4bc2e
pratikunterwegs <pratikgupte16@gmail.com>
2019-05-28 11:12:48 +0200

    add tidal cycle calculations

--- knots_code/codeRawData/code_prepSelectBirds.r
+++ knots_code/codeRawData/code_prepSelectBirds.r
@@ -21,7 +21,9 @@ for(i in 1:length(dataFiles))
     keep(function(x) length(x) > 0) %>% # keep non null lists
     flatten() %>% # flatten this list structure
     keep(function(x) nrow(x) > 0) # keep dfs with data
-    
+ 
+  rm(data2018.raw); gc()
+     
   data <- keep(data, function(x) unique(x$TAG - 3.1001e10) %in% selected_birds)
   
   # if the list is non-empty, add to a another list
@@ -46,3 +48,54 @@ if(!dir.exists("../data2018/selRawData")){
 
 # write to file
 data.table::fwrite(selectData, file = "../data2018/selRawData/birdsForSeg.csv")
+
+# remove select data
+rm(selectData); gc()
+
+#### assign tidal cycles ####
+# load tidal data
+tides <- read_csv("../data2018/tidesSummer2018.csv") %>%
+  filter(tide == "H")
+
+# read in sample data as data
+data <- fread("../data2018/selRawData/birdsForSeg.csv")
+
+# filter out 24 hours after release
+# read in release time
+releaseData <- read_csv("../data2018/behavScores.csv") %>% 
+  mutate(timeNumRelease = as.numeric(Release_Date))
+
+# join to data and keep data where time >= release date + 24 h
+data <- left_join(data, releaseData %>% select(id, timeNumRelease)) %>% 
+  filter(time >= timeNumRelease + (24 * 3600)) %>% 
+  select(-timeNumRelease)
+
+# bind tides and data on time
+data <- split(data, f = data$id) %>% 
+  map(function(df){
+    full_join(df, tides, by = c("time" = "timeNum")) %>% 
+      arrange(time) %>%                                 # arrange by time
+      mutate(tide = ifelse(is.na(level), "other", tide),# classify points
+             tidalCycle = cumsum(tide == "H")) %>%      # assign tidal cycle
+      filter(!is.na(x)) %>%                             # remove non positions
+      select(-tide, -level, -time.y)                    # remove tidal data
+  }) %>% bind_rows() %>% 
+  mutate(tidalCycle = tidalCycle - min(tidalCycle) + 1) %>% 
+  group_by(id, tidalCycle) %>% 
+  mutate(timeToHiTide = (time - min(time)) / 3600)
+
+# plot positions per tidal cycle
+count(data, id, tidalCycle) %>% 
+  ggplot(aes(x = factor(tidalCycle), y = factor(id), fill = n))+
+  geom_tile(col = "white", size = 0.2)+
+  scale_fill_viridis_c(option = "magma")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, size = 4))+
+  labs(x = "tidal cycle", y = "id", fill = "fixes")
+
+# make plot
+ggsave("../figs/figRdSampleFixesPerTide.pdf", device = pdf(), height = 5,
+       width = 12); dev.off()
+
+# write to file
+fwrite(data, file = "../data2018/selRawData/rawdataWithTides.csv")
