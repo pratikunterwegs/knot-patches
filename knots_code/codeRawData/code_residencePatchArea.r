2b494ea58528868aa8c9db431947b1474b452cc5
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-03 15:55:52 +0200

    figure out patch polygonisation

--- knots_code/codeRawData/code_residencePatchArea.r
+++ knots_code/codeRawData/code_residencePatchArea.r
@@ -16,28 +16,42 @@ names <- purrr::map_chr(data,
 names(data) <- names
 
 # prep to assign sequence to res patches
+# to each id.tide combination
 data <- purrr::map(data, function(df) {
+  # remove NA vals in fpt
+  # set residence time to 0 or 1 predicated on <= 10 (mins)
   df[!is.na(fpt),][,resTime:= ifelse(resTime <= 10, F, T)
+                   # get breakpoints where F chances to T and vice versa
                    ][,resPatch:= c(as.numeric(resTime[1]),
                                    diff(resTime))
+                     # keeping fixes where restime > 10
                      ][resTime == T,
+                       # assign res patch as change from F to T
                        ][,resPatch:= cumsum(resPatch)]
 })
 
 # make residence patches
-dataPatch <- list()
 library(sf)
-for(i in 1:length(data[c(1:2)])){
-  df <- data[[i]]
-  
-  df <- group_by(df, resPatch) %>% 
+
+dataPatches <- map(data[c(1:5)], function(df){
+  # group by patch
+  group_by(df, resPatch) %>% 
+    # make sd
     st_as_sf(coords = c("x", "y")) %>% 
-        `st_crs<-`(32631) %>% 
-        st_buffer(10) %>% 
-        group_by(resPatch) %>% 
-    summarise()
-  
-}
+    # assign crs
+    `st_crs<-`(32631) %>% 
+    # draw a 10 m buffer (arbit choice)
+    st_buffer(10) %>% 
+    # dissolve overlapping polygons into each other 
+    st_union() %>% 
+    # split the resulting polygon into its constituents
+    st_cast("MULTIPOLYGON")
+})
+
+distancePatches
+
+# write to file to check if something can be done in qgis
+st_write(df2, dsn = "../data2018/selRawData/patch", layer = "patches.shp", driver = "ESRI Shapefile", delete_layer = TRUE)
 
 
 
