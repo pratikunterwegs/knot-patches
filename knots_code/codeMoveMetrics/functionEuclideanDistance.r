7db29500d831448837a6692e3f353363b838e9a2
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-16 11:08:54 +0200

    update euclidean dist function

--- knots_code/codeMoveMetrics/functionEuclideanDistance.r
+++ knots_code/codeMoveMetrics/functionEuclideanDistance.r
@@ -7,23 +7,14 @@
 # the distance function takes a dataframe, converts to matrix
 # assigns the colnames again as x and y
 
-funcDistance = function(a, x, y){
+funcDistance = function(df, x, y){
   #check for basic assumptions
-  assertthat::assert_that(is.data.frame(a),
+  assertthat::assert_that(is.data.frame(df),
                           is.character(x),
                           is.character(y),
-                          nrow(a) > 1)
-  # subset the dataframe, make matrix, set the new colnames
-  a = as.matrix(a[c(x,y)])
-  colnames(a) = c(x,y)
-  
-  # make a vector with trailing NA to store distances
-  b = vector()
-  for(i in 1:nrow(a)-1){
-    b[i] = (sqrt((a[i,x] - a[i+1, x])^2 + (a[i,y] - a[i+1, y])^2))
-  }
-  b = c(b, NA)
-  
-  # return b
-  return(b)
+                          nrow(df) > 1,
+                          msg = "some df assumptions are not met")
+  dist <- sqrt((df$x[2:length(df$x)] - df$x[1:length(df$x)-1])^2 + 
+                 (df$y[2:length(df$y)] - df$y[1:length(df$y)-1])^2)
+  return(c(NA,dist))
 }
