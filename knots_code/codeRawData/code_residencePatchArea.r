a15b4ef52d6de1a8954efe73a47b4be3d96e115f
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-16 12:23:25 +0200

    remove res patch func as sep file

--- knots_code/codeRawData/code_residencePatchArea.r
+++ knots_code/codeRawData/code_residencePatchArea.r
@@ -3,7 +3,7 @@
 library(tidyverse); library(data.table)
 
 # read in recurse data for selected birds
-dataRevFiles <- list.files("../data2018/selRawData/recurseData/", full.names = T)
+dataRevFiles <- list.files("../data2018/oneHertzDataSubset/recurseData/", full.names = T)
 # read in the data
 data <- purrr::map(dataRevFiles, fread) %>% 
   keep(function(x) { nrow(x) > 0})
@@ -12,7 +12,7 @@ data <- purrr::map(dataRevFiles, fread) %>%
 library(glue)
 names <- purrr::map_chr(data, 
                         function(x){ glue(unique(x$id), 
-                                          stringr::str_pad(unique(x$tidalCycle), 3, pad = 0),
+                                          stringr::str_pad(unique(x$tidalcycle), 3, pad = 0),
                                           .sep = ".") })
 # assign names
 names(data) <- names
@@ -23,7 +23,7 @@ data <- purrr::map(data, function(df) {
   # remove NA vals in fpt
   # set residence time to 0 or 1 predicated on <= 10 (mins)
   df[!is.na(fpt),][,resTime:= ifelse(resTime <= 10, F, T)
-                   # get breakpoints where F chances to T and vice versa
+                   # get breakpoints where F changes to T and vice versa
                    ][,resPatch:= c(as.numeric(resTime[1]),
                                    diff(resTime))
                      # keeping fixes where restime > 10
@@ -33,11 +33,14 @@ data <- purrr::map(data, function(df) {
 }) %>% 
   keep(function(x) nrow(x) > 2)
 
-# get time to high tide
-dataHt <- fread("../data2018/selRawData/rawdataWithTides.csv")
+# get time to high tide from written data
+dataHt <- list.files("../data2018/oneHertzDataSubset/recursePrep/", full.names = T) %>% 
+  map(fread) %>% bind_rows() %>% select(time, id, x, y, tidaltime, dist, tidalcycle)
 
+# merge to recurse data
 data <- map(data, function(df){
-  merge(df, dataHt, all=FALSE)
+  df <- merge(df, dataHt, all = FALSE)
+  return(df)
 })
 
 # clear main data
@@ -56,85 +59,7 @@ dfErrors <- list()
 source("codeMoveMetrics/functionEuclideanDistance.r")
 
 #### function for resPatches arranged by time ####
-funcGetResPatches <- function(df){
-  
-  # assert df is a data frame
-  
-  # try function and ignore errors for now
-  tryCatch(
-    { 
-      # convert to sf
-      pts = df %>% 
-        # make sd
-        st_as_sf(coords = c("x", "y")) %>% 
-        # assign crs
-        `st_crs<-`(32631)#
-      
-      # make polygons
-      polygons = pts %>% 
-        # draw a 50 m buffer (arbitrary choice)
-        st_buffer(10.0) %>%
-        
-        # group_by(resPatch) %>%
-        # summarise()
-        # # dissolve overlapping polygons into each other
-        st_union(by_feature = FALSE) %>%
-        # # split the resulting multipolygon into its constituents
-        st_cast("POLYGON") %>% 
-        st_sf() %>% 
-        mutate(patch = 1:nrow(.))
-      
-      # get which points are covered by which polygon
-      dfOverlaps = as_tibble(st_covers(polygons, pts)) %>% 
-        rename(patch = row.id, point = col.id)
-      
-      # get mean time and time to high tide from base data
-      # make a new tibble as a left join
-      patchMetrics = left_join(dfOverlaps,
-                          # with raw data assigned row ids as point id
-                          df %>% 
-                            mutate(point = 1:nrow(df)) %>% 
-                            select(point, x, y, time, timeToHiTide)) %>% 
-        # for each polygon or residence patch
-        group_by(patch) %>% 
-        # summarise the mean:
-        # x,y (centroid), numeric time, and time since HT
-        summarise_at(vars(x,y,time,timeToHiTide), list(mean = mean)) %>% 
-        
-        # add area: must be here to avoid mixing areas of polygons
-        # as a reordering takes place immediately after
-        # area is in metres squared, the map units (this is UTM zone 31N)
-        mutate(area = as.numeric(st_area(polygons)), 
-               # assign a spatial geometry
-               geom = polygons$geometry) %>% 
-        
-        # join to the polygons spatial data
-        
-        # arrange by time_mean
-        arrange(time_mean) %>%
-        # reorder the polygons and get distance between each and area
-        mutate(patch = 1:nrow(.),
-               distance = funcDistance(., "x_mean", "y_mean")) %>% 
-        # convert result to sf
-        st_sf()
-        
-      
-      # remove data
-      rm(pts, polygons, dfOverlaps); gc()
-        
-      # return the patch data as function output
-      return(patchMetrics)
-    },
-    # null error function, with option to collect data on errors
-    error= function(e)
-    {
-      # print(glue("there was an error in id_tide combination... ",
-      #                             unique(z$id), unique(z$tidalCycle)))
-      # dfErrors <- append(dfErrors, glue(z$id, "_", z$tidalCycle))
-    }
-  )
-  
-}
+source("codeRawData/func_residencePatch.r")
 
 #### get patch data ####
 patchData <- map(data, funcGetResPatches)
@@ -163,7 +88,7 @@ patchData <- pmap(list(patchData, birds, tides), function(df, a, b){
 patchData2 <- sf::st_as_sf(data.table::rbindlist(patchData))
 
 # save as shapefile
-st_write(patchData2, dsn = "../data2018/selRawData/patch", layer = "patches.shp",
+st_write(patchData2, dsn = "../data2018/oneHertzDataSubset/patch", layer = "patches.shp",
          driver = "ESRI Shapefile", delete_layer = TRUE)
 
 # try getting plots
