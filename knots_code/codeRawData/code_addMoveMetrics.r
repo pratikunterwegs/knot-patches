35c2e30fcc864e5bed9de6b03a041f7a5de5972a
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-21 11:34:47 +0200

    substantial changes to move metrics output
    use full data, all birds, all tides, export id and tidal cycle specific data with distance added

--- knots_code/codeRawData/code_addMoveMetrics.r
+++ knots_code/codeRawData/code_addMoveMetrics.r
@@ -6,57 +6,79 @@ library(glue)
 library(fasttime) # fast date-time operations
 
 # select tides
-selected_tides <- c(seq(5, 100, 15))
+# selected_tides <- c(seq(5, 100, 15))
 
 # filter out 24 hours after release
 # read in release time
 releaseData <- fread("../data2018/behavScores.csv", )[,timeNumRelease := as.numeric(fastPOSIXct(Release_Date))]
 
-dataSubset <- map(dataFiles, function(df){
+# get distance function
+source("codeMoveMetrics/functionEuclideanDistance.r")
+
+# list files
+dataFiles <- list.files(path = "../data2018/oneHertzData/", full.names = TRUE)
+
+# make dir for segmentation output
+if(!dir.exists("../data2018/oneHertzData/recursePrep")){
+  dir.create("../data2018/oneHertzData/recursePrep")
+}
+
+# map over all files, filter data for release + 24 hours, add distances
+# and write to file
+map(dataFiles, function(df){
   tempdf <- read_csv(df) %>% setDT() # use readcsv rather than fread
+  
+  print(glue('read in {unique(tempdf$id)}...'))
+  
   newNames <- str_to_lower(names(tempdf))
   setnames(tempdf, newNames)
   
-  tempdf <- tempdf[tidalcycle %in% c(selected_tides),
-                   ][,id:=(tag - 3.1001e10)]
+  tempdf <- tempdf[
+    # tidalcycle %in% c(selected_tides),
+    #                ][
+    ,id:=(tag - 3.1001e10)]
   
-  relTime <- merge(releaseData, tempdf, by = "id", all = FALSE, no.dups = T)$timeNumRelease
+  relTime <- releaseData[releaseData$id == unique(tempdf$id),]$timeNumRelease
   # filter data 24 hours post release time  
-  tempdf <- tempdf[time >= (relTime + 24 * 3600),]
+  tempdf <- tempdf[time >= (relTime + 24 * 3600),] %>% 
+    select(id, tidalcycle, time, x, y, nbs, covxy, tidaltime)
   
   rm(relTime)
   
-  return(tempdf)
-})
-
-# bind and write to file
-fwrite(bind_rows(dataSubset), file = "../data2018/oneHertzDataSubset/data2018oneHzSelTides.csv")
-
-# split data and remove dfs with less than 100 obs
-dataForSeg <- dataSubset %>% keep(function(x) nrow(x) > 100)
-
-source("codeMoveMetrics/functionEuclideanDistance.r")
-# calc distance
-dataDist <- map(dataForSeg, function(df){
+  # group by id and tidal cycle
+  tempdf <- group_by(tempdf, id, tidalcycle) %>% 
+    nest()
   
-  dist <- funcDistance(df, "x", "y")
+  tryCatch(
+    {
+      # get distances
+      tempdf$data <- map(tempdf$data, function(df){
+        mutate(df, dist = funcDistance(df, "x", "y"))
+      })
+    },
+    error = function(e) {print(glue('problems in distance calculation of id {unique(tempdf$id)}'))}
+  )
   
+  # write to file in id and tidal cycle combination
+  pmap(list(tempdf$id, tempdf$tidalcycle, tempdf$data), function(a, b, c) {
+    
+    fwrite(mutate(c, id = a, tidalcycle = b), 
+           file = glue("../data2018/oneHertzData/recursePrep/", a,
+                       "_", str_pad(b, 3, pad = "0")))
+    
+    print(glue('id {unique(a)} in tide {unique(b)} written to file'))
+    
+  })
+  
+  return(glue('id {unique(tempdf$id)} and tidal cycle subsets written to file'))
 })
 
-# add distance to reg data frame
-dataForSeg <- map2(dataForSeg, dataDist, function(z,w){
-  mutate(z, dist = w)
-})
 
-# make dir for segmentation output
-if(!dir.exists("../data2018/oneHertzDataSubset/recursePrep")){
-  dir.create("../data2018/oneHertzDataSubset/recursePrep")
-}
 
-# split by id and tidal cycle
-dataForSeg <- map(dataForSeg, function(x){
-  group_by(x, tidalcycle, id) %>% nest()
-}) %>% bind_rows()
+# # split by id and tidal cycle
+# dataForSeg <- map(dataForSeg, function(x){
+#   group_by(x, tidalcycle, id) %>% nest()
+# }) %>% bind_rows()
 
 # paste id and tide for easy extraction, pad tide number to 3 digits
 library(glue)
