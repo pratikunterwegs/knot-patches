28ab7d3eb2a1a1a0073efb54c326153596f16f5d
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-25 10:27:31 +0200

    changes to patch metric plotting

--- knots_code/codeRawData/code_patchSizeVsTestScore.r
+++ knots_code/codeRawData/code_patchSizeVsTestScore.r
@@ -1,20 +1,16 @@
 #### link patch size to exploration score ####
 
 # load libs
-library(tidyr); library(dplyr); library(readr); library(sf)
+library(tidyr); library(dplyr); library(readr)
 
 # read in patch size data from shapefile
-patches <- st_read("../data2018/oneHertzDataSubset/patch/patches.shp") 
-# make an sf copy
-patchSf <- patches
+patches <- read_csv("../data2018/oneHertzData/data2018patches.csv") 
 
 # read in behav scores
-behavScore <- read_csv("../data2018/behavScores.csv") %>% 
-  mutate(bird = factor(id))
+behavScore <- read_csv("../data2018/behavScores.csv")
 
 # link behav score and patch size and area
-patches <- left_join(patches, behavScore, by= c("bird")) %>% 
-  st_drop_geometry()
+patches <- left_join(patches, behavScore, by= c("id"))
 
 # # filter out unreasonable data of greater than 100 x 100 m
 # patches <- filter(patches)
@@ -24,23 +20,34 @@ library(ggplot2)
 source("codePlotOptions/geomFlatViolin.r")
 source("codePlotOptions/ggThemePub.r")
 
+# simple ci function
+ci = function(x){
+  qnorm(0.975)*sd(x, na.rm = T)/sqrt((length(x)))
+}
+
 #### patch area vs tidal stage over season ####
+patchSummary <- patches %>% 
+  mutate(tidalCluster = plyr::round_any(tidalcycle, 15),
+         tidalTimeHour = plyr::round_any(tidaltime_mean/60, 1)) %>% 
+  filter(tidalTimeHour <= 12, tidalCluster <=120) %>% 
+  group_by(tidalCluster, tidalTimeHour) %>% 
+  summarise_at(vars(area, nFixes, distInPatch, distBwPatch),
+               list(~mean(., na.rm = T), ~ci(.)))
+
 # plot boxplots of foraging vs non-foraging patches
-patches %>% 
-  mutate(highTideHour = floor(tidaltime_mean/60),
-         foraging = ifelse(between(highTideHour, 4, 9), "low-tide", "high-tide")) %>%
-  ggplot()+
-  geom_boxplot(aes(x = factor(highTideHour), 
-                   y = area, fill = highTideHour %in% c(4:9)),
-               position = position_dodge(preserve = "single", width = 1))+
-  facet_wrap(~tidalCycle)+
-  scale_fill_brewer(palette = "Accent", label = c("high tide","low tide"))+
-  themePubLeg()+
-  ylim(0,1e4)+
+ggplot(patchSummary)+
+  geom_pointrange(aes(x = factor(tidalTimeHour), 
+                   y = area_mean, ymin = area_mean - area_ci,
+                   ymax = area_mean + area_ci,
+                   fill = between(tidalTimeHour, 4, 9)), shape = 21, lty = 1)+
+  facet_wrap(~tidalCluster,
+             labeller = )+
+  scale_fill_brewer(palette = "Greys", label = c("high tide","low tide"))+
+  coord_cartesian(ylim = c(1e3, 4e3))+
   labs(x = "hours since high tide", y = bquote("patch area" (m^2)),
-       title = "patch area ~ time since high tide",
+       title = "patch area ~ time since high tide ~ tidal cluster",
        caption = Sys.time(), fill = "rough tidal stage")+
-  theme(legend.position = c(0.9, 0.1))
+  theme(legend.position = c(0.9, 0.9))
 
 ggsave(filename = "../figs/figPatchAreaVsTime.pdf", width = 11, height = 8,
        device = pdf()); dev.off()
@@ -112,8 +119,8 @@ patches %>% #filter(area < 1e5) %>%
   gather(predictor, value, -bird, -area, -tdlCycl) %>% 
   group_by(predictor, bird, value) %>% 
   summarise_at(vars(area), list(mean=mean, sd=sd)) %>% 
-
-ggplot()+
+  
+  ggplot()+
   geom_pointrange(aes(x = value, y = mean, ymin = mean-sd, ymax = mean+sd, group = bird), size= 0.5, alpha = 0.2)+
   geom_smooth(aes(x = value, y = mean), method = "glm")+
   facet_wrap(~predictor, scales = "free_x")+
