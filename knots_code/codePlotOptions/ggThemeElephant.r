853fd94c5998cd3e8c1f6396731185f14121decc
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-25 23:57:57 +0200

    update themes, add ele theme

--- knots_code/codePlotOptions/ggThemeElephant.r
+++ knots_code/codePlotOptions/ggThemeElephant.r
@@ -2,55 +2,58 @@
 # make theme for plots
 library(ggplot2)
 
-themePubGeese <- function(base_size=10) {
+themePubEle <- function(base_size=10) {
   library(grid)
   library(ggthemes)
-  (theme_foundation(base_size=base_size)+
+  (theme_classic(base_size=base_size)+
+  #  theme(plot.title = element_text(size = 12, face = "bold"))
     theme(plot.title = element_text(size = rel(1), face = "bold"),
             text = element_text(),
             panel.background = element_rect(fill = "white", colour = "transparent"),
-            panel.border = element_rect(),
+            panel.border = element_blank(),
             axis.title = element_text(face = "plain",size = rel(1)),
             axis.title.y = element_text(angle=90,vjust =2),
             axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(),
-            axis.line = element_blank(),
-            axis.ticks = element_line(size = 0.2),
+            axis.text.x = element_text(vjust = 0),
+            axis.text.y = element_text(angle=90, vjust = 1, hjust = 0.5),
+            axis.line = element_line(lineend = "round"),
+            axis.ticks = element_line(size = 0.3),
             axis.ticks.length = unit(.2, "cm"),
             panel.grid.major = element_blank(),
             legend.position = "none",
             panel.grid.minor = element_blank(),
-            plot.margin=unit(c(10,5,5,5),"mm"),
-            plot.background = element_rect(fill = "white", colour = "transparent"),
+           plot.margin=unit(c(10,5,5,5),"mm"),
+           plot.background = element_rect(fill = "white", colour = "transparent"),
             strip.background=element_rect(fill = "grey90"),
             strip.text = element_text(face="bold", hjust = 0)
     ))
 
 }
 
-themePubLegGeese <- function(base_size=10) {
+themePubLegEle <- function(base_size=10) {
   library(grid)
   library(ggthemes)
   (theme_foundation(base_size=base_size)+
     theme(plot.title = element_text(size = rel(1), face = "bold"),
             text = element_text(),
             panel.background = element_rect(fill = "white", colour = "transparent"),
-            panel.border = element_rect(),
+            panel.border = element_blank(),
             axis.title = element_text(face = "plain",size = rel(1)),
             axis.title.y = element_text(angle=90,vjust =2),
             axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(colour = "grey40"),
-            axis.line = element_blank(),
-            axis.ticks = element_line(size = 0.2),
+            axis.text.x = element_text(vjust = 0),
+            axis.text.y = element_text(angle=90, vjust = 1, hjust = 0.5),
+            axis.line = element_line(lineend = "round"),
+            axis.ticks = element_line(size = 0.3),
             axis.ticks.length = unit(.2, "cm"),
             panel.grid.major = element_blank(),
             legend.position = "right",
             panel.grid.minor = element_blank(),
-            legend.title = element_text(),
-            legend.background = element_rect(colour = 1, size = 0.2),
-            legend.key = element_blank(),
-            plot.margin=unit(c(10,5,5,5),"mm"),
-            plot.background = element_rect(fill = "white", colour = "transparent"),
+          legend.title = element_text(),
+          legend.background = element_rect(colour = 1, size =0.2),
+          legend.key = element_blank(),
+           plot.margin=unit(c(10,5,5,5),"mm"),
+           plot.background = element_rect(fill = "white", colour = "transparent"),
             strip.background=element_rect(fill = "grey90"),
             strip.text = element_text(face="bold", hjust = 0)
     ))
--- knots_code/codePlotOptions/ggThemeKnots.r
+++ knots_code/codePlotOptions/ggThemeKnots.r
@@ -6,33 +6,22 @@ themePubKnots <- function(base_size=10) {
   library(grid)
   #require(lemon)
   (theme_classic(base_size=base_size)+
-      #  theme(plot.title = element_text(size = 12, face = "bold"))
       theme(plot.title = element_text(size = rel(1), face = "bold"),
             text = element_text(),
-            #panel.background = element_rect(fill = "white", colour = 1),
             panel.border = element_blank(),
             axis.title = element_text(face = "plain",size = rel(1)),
             axis.title.y = element_text(angle=90,vjust = 4),
             axis.title.x = element_text(vjust = -2),
             axis.text.x = element_text(vjust = -1),
             axis.text.y = element_text(angle=90, vjust = 2, hjust = 0.5),
-            #axis.line = element_line(),
             axis.ticks = element_line(size = 0.3),
             axis.ticks.length = unit(-0.1, "cm"),
             panel.grid.major = element_blank(),
             legend.position = "none",
-            #element_line(size = 0.2, colour = "grey80"),
             panel.grid.minor = element_blank(),
-            #  legend.key = element_rect(colour = NA),
-            #  legend.position = "bottom",
-            #  legend.direction = "horizontal",
-            #  legend.key.size= unit(0.2, "cm"),
-            #  legend.margin = unit(0, "cm"),
-            # legend.title = element_text(face="italic"),
             plot.margin=unit(c(10,5,5,5),"mm"),
             plot.background = element_rect(fill = "white", colour = "transparent"),
             strip.background=element_rect(fill = "grey90", colour = "transparent"),
-            #element_rect(colour="#f0f0f0",fill="#f0f0f0"),
             strip.text = element_text(face="bold", hjust = 0)
       ))
   
@@ -40,39 +29,26 @@ themePubKnots <- function(base_size=10) {
 
 themePubLegKnots <- function(base_size=10) {
   library(grid)
-  #require(lemon)
   (theme_classic(base_size=base_size)+
-      #  theme(plot.title = element_text(size = 12, face = "bold"))
       theme(plot.title = element_text(size = rel(1), face = "bold"),
             text = element_text(),
-            #panel.background = element_rect(fill = "white", colour = 1),
             panel.border = element_blank(),
             axis.title = element_text(face = "plain",size = rel(1)),
             axis.title.y = element_text(angle=90,vjust = 4),
             axis.title.x = element_text(vjust = -2),
             axis.text.x = element_text(vjust = -1),
             axis.text.y = element_text(angle=90, vjust = 2, hjust = 0.5),
-            #axis.line = element_line(),
             axis.ticks = element_line(size = 0.3),
             axis.ticks.length = unit(-0.1, "cm"),
             panel.grid.major = element_blank(),
             legend.position = "right",
-          #element_line(size = 0.2, colour = "grey80"),
             panel.grid.minor = element_blank(),
-            #legend.key = element_rect(colour = 1),
-          #  legend.position = "bottom",
-          #  legend.direction = "horizontal",
-          #  legend.key.size= unit(0.2, "cm"),
-          #  legend.margin = unit(0, "cm"),
           legend.title = element_text(),
-          legend.background = element_rect(colour = 1),
+          legend.background = element_rect(colour = 1, size = 0.2),
           legend.key = element_blank(),
-            #  legend.margin = unit(0, "cm"),
-            # legend.title = element_text(face="italic"),
             plot.margin=unit(c(10,5,5,5),"mm"),
             plot.background = element_rect(fill = "white", colour = "transparent"),
             strip.background=element_rect(fill = "grey90", colour = "transparent"),
-            #element_rect(colour="#f0f0f0",fill="#f0f0f0"),
             strip.text = element_text(face="bold", hjust = 0)
       ))
   
