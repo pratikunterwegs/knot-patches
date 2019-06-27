52d7583ec9dacc6410c22643d69b08283947578f
Pratik Gupte <pratikgupte16@gmail.com>
2019-06-27 20:03:31 +0200

    complete mcp area code

--- knots_code/codeRawData/code_distanceMCP.r
+++ knots_code/codeRawData/code_distanceMCP.r
@@ -2,23 +2,33 @@
 
 library(tidyverse); library(data.table)
 
+library(sf)
+
 # list files
 dataFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)
 
-data <- map_df(dataFiles, function(filename){
+data <- map_df(dataFiles[1:10], function(filename){
   
+  # place a try catch here
   # read in data
   df <- fread(filename)[!is.na(dist),.(x,y,time,id, tidalcycle, dist)]
+  
+  # make sf, union, get convex hull area
+  mcp <- st_as_sf(df, coords = c("x","y")) %>% st_union() %>% st_convex_hull() %>% st_area()
+  
   # sum distance
-  df[,.(totalDist = sum(dist, na.rm = T)), by=.(id, tidalcycle)]
+  df <- df[,.(totalDist = sum(dist, na.rm = T)), by=list(id, tidalcycle)]
+  
+  # add area
+  df[,mcpArea:=mcp]
   
   # make df
   setDF(df)
   
+  # return
+  return(df)
+  
 })
 
-# get MCPs
-data_mcp <- map(dataFiles, function(filename){
-  # read in data
-  df <- setDF(fread(filename)[,]
-}) 
+# write to file
+fwrite(data, file = "../data2018/oneHertzData/dataMCParea.csv")
