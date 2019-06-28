1b047140d1c40737867082692320cfbca6f607e6
pratikunterwegs <pratikgupte16@gmail.com>
2019-06-28 10:35:30 +0200

    add plot df and code

--- knots_code/codeRawData/code_distanceMCP.r
+++ knots_code/codeRawData/code_distanceMCP.r
@@ -39,5 +39,49 @@ data <- map(dataFiles, function(filename){
   
 })
 
+# filter data because there were problems with 1 row data
+data <- keep(data, function(z){
+  # check if both mcp area and dist are present
+  sum(c("totalDist", "mcpArea") %in% names(z)) == 2
+})
+
+# bind to df
+data <- bind_rows(data)
+
 # write to file
 fwrite(data, file = "../data2018/oneHertzData/dataMCParea.csv")
+
+#### plot mcp and distance vs explore score ####
+# read in behav scores
+behavScore <- read_csv("../data2018/behavScores.csv")
+
+# pot ops
+source("codePlotOptions/ggThemeKnots.r")
+
+# simple ci function
+ci = function(x){
+  qnorm(0.975)*sd(x, na.rm = T)/sqrt((length(x)))
+}
+
+# join explore score and area dist data
+data = inner_join(data, behavScore, by = "id")
+
+# get summary, all in KILOMETRES OR KM^2
+dataSummary = data %>% 
+  mutate(mcpArea = mcpArea/(1e3*1e3), totalDist = totalDist/1e3,
+         exploreBin = plyr::round_any(exploreScore, 0.2)) %>% 
+  select(totalDist, mcpArea, exploreBin) %>% 
+  gather(variable, value, -exploreBin) %>% 
+  group_by(exploreBin, variable) %>% 
+  summarise_at(vars(value), list(~mean(.), ~ci(.)))
+
+# plot
+ggplot(dataSummary)+
+  geom_pointrange(aes(x = exploreBin, y = mean, ymin=mean-ci, ymax=mean+ci))+
+  facet_wrap(~variable, scales = "free")+
+  scale_x_continuous(breaks = unique(dataSummary$exploreBin))+
+  themePubLegKnots()+
+  labs(x = "exploration score", y = bquote("mean ± 95% CI (km or km"^2~")"))
+
+# save plot
+ggsave(filename = "../figs/figMCPareaExploreScore.pdf", height = 3, width = 6)
