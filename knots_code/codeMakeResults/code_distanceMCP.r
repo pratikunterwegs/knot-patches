af6f4c0da6924404ab8c93e7a4921275d6a26006
pratikunterwegs <pratikgupte16@gmail.com>
2019-07-04 19:44:30 +0200

    amend mcp code to export summed mcp

--- knots_code/codeMakeResults/code_distanceMCP.r
+++ knots_code/codeMakeResults/code_distanceMCP.r
@@ -20,7 +20,10 @@ data <- map(dataFiles, function(filename){
   
   tryCatch(
     # make sf, union, get convex hull area
-    {mcp <- st_as_sf(df, coords = c("x","y")) %>% st_union() %>% st_convex_hull() %>% st_area()},
+    {mcp <- st_as_sf(df, coords = c("x","y")) %>% st_union() %>% 
+      st_convex_hull() %>% `st_crs<-`(32631)
+    
+    mcpArea <- as.numeric(st_area(mcp))},
     error = function(e){ print(glue('problems with bird {unique(df$id)} in tide {unique(df$tidalcycle)}'))})
   
   tryCatch(
@@ -29,10 +32,12 @@ data <- map(dataFiles, function(filename){
   error = function(e){ print(glue('problems with bird {unique(df$id)} in tide {unique(df$tidalcycle)}'))})
   
   # add area
-  df[,mcpArea:=mcp]
+  df[,mcpArea:=mcpArea]
   
   # make df
   setDF(df)
+  
+  df <- cbind(df, mcp) %>% st_as_sf()
   
   # return
   return(df)
@@ -45,11 +50,19 @@ data <- keep(data, function(z){
   sum(c("totalDist", "mcpArea") %in% names(z)) == 2
 })
 
+# export to shapefile
+mcpBirds <- reduce(data, rbind)
+# get bounding box
+bbox <- mcpBirds %>% st_union()
+
+st_write(bbox, dsn = "../data2018/spatials/newUnionPatches", layer = "unionPatches.shp",
+         driver = "ESRI Shapefile")
+
 # bind to df
-data <- bind_rows(data)
+data <- map(data, st_drop_geometry) %>% bind_rows()
 
 # write to file
-fwrite(data, file = "../data2018/oneHertzData/dataMCParea.csv")
+fwrite(data, file = "../data2018/dataMCParea.csv")
 
 #### plot mcp and distance vs explore score ####
 # read in behav scores
@@ -66,22 +79,7 @@ ci = function(x){
 # join explore score and area dist data
 data = inner_join(data, behavScore, by = "id")
 
-# get summary, all in KILOMETRES OR KM^2
-dataSummary = data %>% 
-  mutate(mcpArea = mcpArea/(1e3*1e3), totalDist = totalDist/1e3,
-         exploreBin = plyr::round_any(exploreScore, 0.2)) %>% 
-  select(totalDist, mcpArea, exploreBin) %>% 
-  gather(variable, value, -exploreBin) %>% 
-  group_by(exploreBin, variable) %>% 
-  summarise_at(vars(value), list(~mean(.), ~ci(.)))
+# write to file
+fwrite(data, file = "../data2018/dataMCParea.csv")
 
-# plot
-ggplot(dataSummary)+
-  geom_pointrange(aes(x = exploreBin, y = mean, ymin=mean-ci, ymax=mean+ci))+
-  facet_wrap(~variable, scales = "free")+
-  scale_x_continuous(breaks = unique(dataSummary$exploreBin))+
-  themePubLegKnots()+
-  labs(x = "exploration score", y = bquote("mean ± 95% CI (km or km"^2~")"))
-
-# save plot
-ggsave(filename = "../figs/figMCPareaExploreScore.pdf", height = 3, width = 6)
+# end here
