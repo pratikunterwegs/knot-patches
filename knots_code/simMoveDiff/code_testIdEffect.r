70d2d50fe0cc3c4168b70b99653223d4cbd59f8a
pratikunterwegs <pratikgupte16@gmail.com>
2019-07-15 15:51:44 +0200

    prep for model of id - predictor comparison

--- knots_code/simMoveDiff/code_testIdEffect.r
+++ knots_code/simMoveDiff/code_testIdEffect.r
@@ -3,6 +3,7 @@
 # load libs
 library(data.table)
 library(tidyr); library(dplyr); library(purrr)
+library(stringr)
 
 # source movement lib
 source("codeMoveMetrics/functionEuclideanDistance.r")
@@ -11,22 +12,58 @@ source("codeMoveMetrics/functionEuclideanDistance.r")
 ci = function(x){
   qnorm(0.975)*sd(x, na.rm = T)/sqrt((length(x)))}
 
-# read data
+# read data and nest, separating into random walk and levy flight data frames
 data <- fread("simMoveDiff/dataSimMoveDiff.csv") %>% 
   setDF() %>% 
-  group_by(replicate, id, moveScale) %>% 
-  nest() %>% 
-  mutate(data = map(data, funcDistance)) %>% 
-  unnest() %>%
-  drop_na() %>% 
-  group_by(moveScaleRound = round(moveScale)) %>% 
-  summarise_at(vars(data), list(~mean(.), ~ci(.)))
+  group_by(id, replicate, moveProb, moveScale) %>% nest() %>% 
+  # separate using select
+  mutate(Rw = map(data, function(df){
+    df %>% select(-matches(c("Lf"))) %>% rename("x" = "xRw", "y"="yRw") %>% 
+      mutate(simtype = "rw")
+  }),
+  Lf = map(data, function(df){
+    df %>% select(-matches(c("Rw"))) %>% rename("x" = "xLf", "y" = "yLf") %>% 
+      mutate(simtype = "lf")
+  })) %>% 
+  # gather by sim type
+  select(-data) %>% 
+  gather(sim, data, -id, -replicate, -moveProb, -moveScale)
 
-# plot data
-library(ggplot2)
+# subsample data at 10 - 60% to mimic imperfect sampling  
+data <- data %>% 
+  mutate_at(vars(data), list(~map(., function(df){
+    sample_frac(df, runif(1, 0.1, 0.6))})))
 
-ggplot(data)+
-  geom_pointrange(aes(x=moveScaleRound, y=mean, ymin=mean-ci, ymax=mean+ci))+
-  geom_abline(slope = 0.1)
+# calculate total distance, MCP area, nFixes as for emp data
+library(sf)
 
+dataSummary <- data %>% 
+  # on each of the two kinds of sim data
+  mutate_at(vars(data), 
+            # map the following funcs
+            list(#~map_chr(., function(df){unique(df$simtype)}),
+                 ~map_int(., nrow),
+                 ~map_dbl(., function(df){
+                   # sum the distance
+                   sum(funcDistance(df), na.rm = T)
+                 }),
+                 ~map_dbl(., function(df){
+                   # get the MCP area
+                   st_as_sf(df, coords = c("x","y")) %>% 
+                     st_union() %>% 
+                     st_convex_hull() %>% 
+                     st_area()
+                 }))) %>% 
+  select(-data)
 
+# rename cols
+names(dataSummary) <- c("id", "replicate", "moveProb", "moveScale",
+                        "simtype", "fixes","distance","area")
+
+# prep for model
+modData <- dataSummary %>% 
+  # gather by response variable
+  gather(respvar, simval, -id, -replicate, -fixes, -moveProb, -moveScale, -simtype) %>% 
+  # group by sim type and resp and nest, allows same model code on each df
+  group_by(simtype, respvar) %>% 
+  nest()
