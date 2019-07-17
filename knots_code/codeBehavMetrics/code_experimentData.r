6793f58e1df8e090175573c22326586bbb535672
pratikunterwegs <pratikgupte16@gmail.com>
2019-07-17 16:37:04 +0200

    mod code for behav data

--- knots_code/codeBehavMetrics/code_experimentData.r
+++ knots_code/codeBehavMetrics/code_experimentData.r
@@ -1,35 +1,12 @@
 #### read experiment data and output clean behav scores ####
 
-library(tidyverse); library(readr)
+library(readr)
 
 # read data
-dataExp = read_csv("../data2018/SelinDB.csv")
+dataExp = read_csv("../data2018/Selindb-ranef.csv")
 
-# keep cols and rename others
-colsToKeep = c("SEX","AGE","WING","BILL","TOTHD","TARS","MASS",
-               "gizzard_mass", "pectoral", "Release_Date", "Release_Time")
-
-# subset data for  toa tags
-behavScores = dataExp %>% 
-  select(id = Toa_Tag, colsToKeep, contains("texpl"),
-        -contains("W0")) %>% 
-  filter(!is.na(id))
-
-# convert release date to posixct
-behavScores$Release_Date = as.POSIXct(paste(behavScores$Release_Date,
-                                            behavScores$Release_Time,
-                                            sep = " "),
-                                      format = "%d.%m.%y %H:%M:%S", tz = "Europe/Berlin")
-
-# get mean of field and wadunit scores
-behavScores$exploreScore = behavScores %>% 
-  select(contains("F0")) %>% 
-  apply(1, function(x) mean(x, na.rm = T))
-
-# remove field scores
-behavScores = select(behavScores, -contains("F0"))
+# select col and save
+behavScores = select(dataExp, grp, condval)
 
 # export for use
-write_csv(behavScores, path = "../data2018/behavScores.csv")
-
-
+write_csv(behavScores, path = "../data2018/behavScoresRanef.csv")
