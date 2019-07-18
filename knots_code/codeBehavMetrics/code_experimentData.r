1dbd33dde834647affc62957eeb15ae9897f2814
pratikunterwegs <pratikgupte16@gmail.com>
2019-07-18 09:29:25 +0200

    read different explore scores

--- knots_code/codeBehavMetrics/code_experimentData.r
+++ knots_code/codeBehavMetrics/code_experimentData.r
@@ -1,9 +1,24 @@
 #### read experiment data and output clean behav scores ####
 
-library(readr)
+library(tidyverse)
 
 # read data
-dataExp = read_csv("../data2018/Selindb-ranef.csv")
+# conditional rand effects
+dataRanef = read_csv("../data2018/Selindb-ranef.csv") %>% 
+  select(-grpvar, -term, FB = grp, ranefScore = condval, ranefSd = condsd)
+
+# transformed explore
+dataTexpl = read_delim("../data2018/Selindb-updated_2019-07-17.csv", delim = ";") %>% 
+  filter(trial == "F01") %>% 
+  rename("tExplScore" = "texpl")
+
+# morphometric measures
+dataMorph = read_csv("../data2018/SelinDB.csv") %>% 
+  select(-contains("0"), FB = RINGNR, -X1, status = info, id = Toa_Tag)
+
+# join data
+dataExp = inner_join(dataMorph, dataTag) %>% 
+  inner_join(dataRanef) %>% inner_join(dataTexpl)
 
 # select col and save
 behavScores = select(dataExp, grp, condval)
