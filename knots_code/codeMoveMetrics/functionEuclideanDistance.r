86cbb9d88ea0ef1e1558fc812154c7f9aca90878
pratikunterwegs <pratikgupte16@gmail.com>
2019-08-20 19:54:09 +0200

    clean distance func and test it

--- knots_code/codeMoveMetrics/functionEuclideanDistance.r
+++ knots_code/codeMoveMetrics/functionEuclideanDistance.r
@@ -13,7 +13,6 @@
 # assigns the colnames again as x and y
 
 # needs dplyr
-
 library(dplyr)
 
 funcDistance = function(df, a = "x", b = "y"){
@@ -22,17 +21,22 @@ funcDistance = function(df, a = "x", b = "y"){
                           is.character(a),
                           is.character(b),
                           msg = "some df assumptions are not met")
+  # check for dataframe length
+  assertthat::assert_that(nrow(df) > 1,
+                          msg = "dataframe for distance has too few rows")
   
   dist <- dplyr::case_when(nrow(df) > 1 ~ 
-                      # get x and y
-                      {x <- dplyr::pull(df, a); x1 <- x[1:length(x)-1]; x2 <- x[2:length(x)]
-                      y <- dplyr::pull(df, b); y1 <- y[1:length(y)-1]; y2 <- y[2:length(y)]
-                      
-                      # get dist
-                      c(NA, sqrt((x1 - x2)^2 + (y1 - y2)^2))},
-                    
-                    nrow(df) == 1 ~ {0.0},
-                    TRUE ~ {as.numeric(NA)})
+                             # get x and y
+                             {{x <- dplyr::pull(df, a); 
+                             x1 <- x[1:length(x)-1]; 
+                             x2 <- x[2:length(x)]}
+                             {y <- dplyr::pull(df, b); 
+                             y1 <- y[1:length(y)-1]; 
+                             y2 <- y[2:length(y)]}
+                             # get dist
+                             c(NA, sqrt((x1 - x2)^2 + (y1 - y2)^2))},
+                           nrow(df) == 1 ~ {0.0},
+                           TRUE ~ {as.numeric(NA)})
   
   return(dist)
 }
