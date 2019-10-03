e52fda8927adb00fcb84a8239f8735aad2d0650b
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-03 14:34:42 +0200

    implement multiple param assump segment constr

--- knots_code/codeMoveMetrics/code_testMethod.r
+++ knots_code/codeMoveMetrics/code_testMethod.r
@@ -10,17 +10,33 @@ source("codeMoveMetrics/functionEuclideanDistance.r")
 # function for resPatches arranged by time
 source("codeMakeResults/func_residencePatch.r")
 
+# add a mode function
+
+
 # read in recurse data for selected birds
 dataRevFiles <- list.files("../data2018/oneHertzData/recurseData/", full.names = T)[1]
 
 # get time to high tide from written data
 dataHtFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)[1]
 
-# read in subset of data
-data <- purrr::map2_df(dataRevFiles, dataHtFiles, function(filename, htData){
+# make dataframe of assumption parameters
+resTimeLimit = c(2, 4, 10); travelSeg = c(1, 5, 10); travelTime = c(1, 2, 5, 10)
+assumpData <- crossing(resTimeLimit, travelSeg)
+
+# make data - param assump combo df
+dataToTest <- tibble(revdata = dataRevFiles, htData = dataHtFiles, assump = list(assumpData)) %>% 
+  unnest(cols = assump)
+
+#### testing the patches produced by different assumptions ####
+
+# passing data to a function that manually segments and returns patches
+data <- purrr::pmap(dataToTest, function(revdata, htData, resTimeLimit, travelSeg){
+  
+  # print param assumpts
+  print(glue('param assumpts...\n residence time threshold = {resTimeLimit}\n travel segment smoothing = {travelSeg}'))
   
   # read the file in
-  df <- fread(filename)
+  df <- fread(revdata)
   
   print(glue('individual {unique(df$id)} in tide {unique(df$tidalcycle)} has {nrow(df)} obs'))
   
@@ -28,9 +44,22 @@ data <- purrr::map2_df(dataRevFiles, dataHtFiles, function(filename, htData){
   # to each id.tide combination
   # remove NA vals in fpt
   # set residence time to 0 or 1 predicated on <= 10 (mins)
-  df <- df[!is.na(fpt),][,resTime:= ifelse(resTime <= 2, F, T)
-                         # get breakpoints where F changes to T and vice versa
-                         ][,resPatch:= c(as.numeric(resTime[1]),
+  df <- df[!is.na(fpt),
+           ][,resTime:= ifelse(resTime <= resTimeLimit, F, T)
+                         # get breakpoints if the mean over rows of length travelSeg
+                         # is below 0.5
+             # how does this work?
+             # a sequence of comparisons, resTime <= resTimeLimit
+             # may be thus: c(T,T,T,F,F,T,T)
+             # indicating two non-residence points between 3 and 2 residence points
+             # the rolling mean over a window of length 3 will be
+             # c(1,.67,.33,.33,.33,.67) which can be used to
+             # smooth over false negatives of residence
+                         ][,rollResTime:=(zoo::rollmean(resTime, k = travelSeg, fill = NA) > 0.5)
+                           # drop NAs in rolling residence time evaluation
+                           # essentially the first and last elements will be dropped
+                           ][!is.na(rollResTime),
+                             ][,resPatch:= c(as.numeric(resTime[1]),
                                          diff(resTime))
                            # keeping fixes where restime > 10
                            ][resTime == T,
@@ -42,19 +71,49 @@ data <- purrr::map2_df(dataRevFiles, dataHtFiles, function(filename, htData){
   # merge to recurse data
   df <- merge(df, dataHt, all = FALSE)
   
-  # get patch data
-  patchData <- funcGetResPatches(df)
+  # add param assumptions
+  df$resTimeLimit = resTimeLimit; df$travelSeg = travelSeg
   
-  # remove htData
-  rm(htData)
-  
-  patchData$data <- NULL
-  
-  return(patchData)
+  return(df)
   
 })
 
-#### diagnostic plots ####
+#### diagnostic plots for respatches ####
 # get griend
 griend <- st_read("../griend_polygon/griend_polygon.shp")
+{
+  x11()
+  par(mfrow = c(3,3))
+  map(data, function(df){
+    # plot(griend, col = "grey90", plot.max = 1,
+    #      main = glue('resTimeLimit = {unique(df$resTimeLimit)}\ntravelSeg = {unique(df$travelSeg)}'))
+    
+    setDT(df)
+    setorder(df, time)
+    
+    plot(df$x, df$y, type = "p", add = TRUE, cex = 0.2,
+         col = pals::kovesi.rainbow(20)[df$resPatch])
+  })
+}
+
+
+#### separate funciton to return res patches ####
+funcReturnPatchData <- function(segData){
+  # get patch data
+  patchData <- funcGetResPatches(segData)
+  
+  # remove what seems to be the sf data
+  patchData <- patchData %>% dplyr::select(-data)
+  
+  # add the parameter assumptions
+  patchData$resTimeLimit = resTimeLimit
+  patchData$travelSeg = travelSeg
+  
+  return(patchData)
+}
+
+# get patches
+patches <- map(data, funcReturnPatchData)
+
+
 
