774e6aa86bd7b13dd3f7e55113dc0ee35d94bde9
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-04 11:10:06 +0200

    add plot arrows

--- knots_code/codeMoveMetrics/code_plotResPatches.r
+++ knots_code/codeMoveMetrics/code_plotResPatches.r
@@ -3,7 +3,11 @@
 # load libs
 library(ggplot2)
 
-ggplot(patchSummary)+
-  geom_path(aes(x_mean, y_mean, col = c(distBwPatch[-1],NA)), size =2)+
+ggplot(a)+
+  geom_path(aes(x_mean, y_mean, col = c(distBwPatch[-1],NA)), 
+            #size =2, 
+            arrow = arrow(type = "closed", angle = 10))+
   geom_point(aes(x_mean, y_mean, size = duration), pch = 21, fill = "white")+
-  scale_colour_distiller(palette = "Reds", direction = 1)
+  scale_colour_distiller(palette = "Reds", direction = 1)+
+  theme_void()+
+  labs(col = "distance (m)")
