78f4874a85cff0aace94185dc88476802e091013
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-14 11:18:51 +0200

    add from test code

--- knots_code/codeMakeResults/code_residencePatchMetrics.r
+++ knots_code/codeMakeResults/code_residencePatchMetrics.r
@@ -19,18 +19,27 @@ source("codeFunctions/func_residencePatch.r")
 
 
 # read in recurse data for selected birds
-dataRevFiles <- list.files("../data2018/oneHertzData/recurseData/", full.names = T)
+dataRevFiles <- list.files("../data2018/oneHertzData/recurseData/", full.names = T)[1:3]
 
 # get time to high tide from written data
-dataHtFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)
+dataHtFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)[1:3]
 
-# gather in a dataframe
-data <- as_tibble(dataRevFiles, dataHtFiles)
+# gather in a dataframe# make dataframe of assumption parameters
+resTimeLimit = c(4); travelSeg = c(5)
+assumpData <- crossing(resTimeLimit, travelSeg)
+
+# make data - param assump combo df
+dataToTest <- tibble(revdata = dataRevFiles, 
+                     htData = dataHtFiles, 
+                     assump = list(assumpData)) %>% 
+  unnest(cols = assump)
 
 # read in the data and perform segmentation
-data <- purrr::pmap_df(data, funcSegPath)
+data <- pmap(dataToTest, funcSegPath)
 
-
+# run the patch metric calculations
+# do not return sf
+patches <- map_df(data, funcGetResPatches)
 
 # write data to file
 fwrite(data, file = "../data2018/oneHertzData/data2018patches.csv",
