a82846c580c01c57999c370aa051be00f863813a
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-16 18:19:59 +0200

    use new watlas function

--- knots_code/codeMakeResults/code_residencePatchMetrics.r
+++ knots_code/codeMakeResults/code_residencePatchMetrics.r
@@ -8,38 +8,26 @@
 library(tidyverse); library(data.table)
 library(glue); library(sf)
 
-# source distance function
-source("codeFunctions/functionEuclideanDistance.r")
-
-# source segmentation function
-source("codeFunctions/func_segmentPath.r")
-
-# function for resPatches arranged by time
-source("codeFunctions/func_residencePatch.r")
-
+# load the custom library
+devtools::install_github("pratikunterwegs/watlasUtils", force = TRUE)
+library(watlasUtils)
 
 # read in recurse data for selected birds
-dataRevFiles <- list.files("../data2018/oneHertzData/recurseData/", full.names = T)[1:3]
+dataRevFiles <- list.files("../data2018/oneHertzData/recurseData/", full.names = T)
 
-# get time to high tide from written data
-dataHtFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)[1:3]
-
-# gather in a dataframe# make dataframe of assumption parameters
-resTimeLimit = c(4); travelSeg = c(5)
-assumpData <- crossing(resTimeLimit, travelSeg)
-
-# make data - param assump combo df
-dataToTest <- tibble(revdata = dataRevFiles, 
-                     htData = dataHtFiles, 
-                     assump = list(assumpData)) %>% 
-  unnest(cols = assump)
+# read in ht data
+dataHtFiles <- list.files("../data2018/oneHertzData/recursePrep/", full.names = T)
 
 # read in the data and perform segmentation
-data <- pmap(dataToTest, funcSegPath)
+data <- map2(dataRevFiles, dataHtFiles, function(df1, df2){
+  watlasUtils::funcSegPath(revdata = df1, htdata = df2)
+})
 
 # run the patch metric calculations
 # do not return sf
-patches <- map_df(data, funcGetResPatches)
+patches <- map_df(data, function(onThisData){
+  watlasUtils::funcGetResPatches(df = onThisData)
+})
 
 # write data to file
 fwrite(data, file = "../data2018/oneHertzData/data2018patches.csv",
