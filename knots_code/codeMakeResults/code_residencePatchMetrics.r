b71dad7a8c5e3e767b413b07ffb02bc946738028
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-18 13:16:13 +0200

    remove empty data

--- knots_code/codeMakeResults/code_residencePatchMetrics.r
+++ knots_code/codeMakeResults/code_residencePatchMetrics.r
@@ -23,10 +23,14 @@ data <- map2(dataRevFiles, dataHtFiles, function(df1, df2){
   watlasUtils::funcSegPath(revdata = df1, htdata = df2)
 })
 
+
+# remove data with fewer than 5 rows
+data <- purrr::keep(data, function(df) nrow(df) > 0 & !is.na(nrow(df)))
+
 # run the patch metric calculations
 # do not return sf
 patches <- map_df(data, function(onThisData){
-  watlasUtils::funcGetResPatches(df = onThisData)
+  watlasUtils::funcGetResPatches(df = onThisData, returnSf = FALSE)
 })
 
 # test some patches
