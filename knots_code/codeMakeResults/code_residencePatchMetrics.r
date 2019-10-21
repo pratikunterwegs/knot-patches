7ca5100849008e8a2affaa76eb52e16ff20b6567
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-21 08:29:09 +0200

    process data of fixed quality

--- knots_code/codeMakeResults/code_residencePatchMetrics.r
+++ knots_code/codeMakeResults/code_residencePatchMetrics.r
@@ -27,9 +27,12 @@ if(!dir.exists("../data2018/segmentData")){
 map2(dataRevFiles, dataHtFiles, function(df1, df2){
   # make segmented data
   somedata <- watlasUtils::funcSegPath(revdata = df1, htdata = df2)
-  # write segmented data
-  fwrite(x = somedata, file = glue::glue('../data2018/segmentData/seg_{unique(somedata$id)}_{unique(somedata$tidalcycle)}.csv'), dateTimeAs = "epoch")
-
+  
+  if(nrow(somedata) > 5 & !is.na(nrow(somedata))){
+    
+    # write segmented data
+    fwrite(x = somedata, file = glue::glue('../data2018/segmentData/seg_{unique(somedata$id)}_{unique(somedata$tidalcycle)}.csv'), dateTimeAs = "epoch")
+  }
 })
 
 gc()
@@ -51,30 +54,33 @@ if(!dir.exists("../data2018/patchData")){
 map(segFiles, function(onThisData){
   # read in data
   data <- fread(onThisData)
-
+  
+  # prop inf
+  preal <- data[,.N,by="type"][,p:=N/sum(N)][type=="real",p]
+  
   # look if data are present
-  if(nrow(data) > 0 & !is.na(nrow(data))){
+  if(preal >= 0.2){
     
     # run patch function if
     patches <- watlasUtils::funcGetResPatches(df = data, returnSf = FALSE)
-
+    
     # write data
     fwrite(x = patches, file = glue::glue('../data2018/patchData/patches_{unique(data$id)}_{unique(data$tidalcycle)}.csv'), dateTimeAs = "epoch")
   }
-
+  
 })
 
 # test some patches
 library(ggplot2)
 library(ggthemes)
 ggplot(patches)+
-geom_point(aes(X_mean,Y_mean, size = duration, col = type))+
-geom_path(aes(X_mean,Y_mean), arrow = arrow(angle = 7))+
-theme(legend.position = "none")
+  geom_point(aes(X_mean,Y_mean, size = duration, col = type))+
+  geom_path(aes(X_mean,Y_mean), arrow = arrow(angle = 7))+
+  theme(legend.position = "none")
 
 # write data to file
 fwrite(patches, file = "../data2018/oneHertzData/data2018patches.csv",
-dateTimeAs = "epoch")
+       dateTimeAs = "epoch")
 
 
 # end here
