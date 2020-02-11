5623cf5fd39513d0711612dfdd3767436aebdbea
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-11 17:58:09 +0100

    some changes to func patches

--- code/func_patches_2018.r
+++ code/func_patches_2018.r
@@ -16,10 +16,14 @@ library(glue)
 library(stringr)
 
 process_patches_2018 <- function(df){
+
+  # global counter for success
+  success = FALSE
   
   {
     temp_data <- fread(df)
     temp_data[,ts:=fastPOSIXct(ts)]
+    orig_data <- wat_agg_data(temp_data, interval = 30)
         
     id <- unique(temp_data$id)
     tide_number <- unique(temp_data$tide_number)
@@ -41,7 +45,7 @@ process_patches_2018 <- function(df){
         patch_data <- wat_make_res_patch(somedata = temp_data,
                                          bufferSize = 10,
                                          spatIndepLim = 100,
-                                         tempIndepLim = 30,
+                                         tempIndepLim = 10,
                                          restIndepLim = 30,
                                          minFixes = 3,
                                          tideLims = c(4,10))
@@ -52,58 +56,88 @@ process_patches_2018 <- function(df){
                                             whichData = "summary")
 
         # get patch data points
-        
+        patch_points <- wat_get_patch_summary(resPatchData = patch_data,
+                                            dataColumn = "data",
+                                            whichData = "points")
+
         # print message
         message(as.character(glue('patches {id}_{tide_number} done')))
         
-        return(patch_data)
+        success = TRUE
       },
       # null error function, with option to collect data on errors
       error= function(e)
       {
-        ### MUST RETURN NA VALUE
         message(glue::glue('patches {id}_{tide_number} errored'))
       }
       
     )
   }
+
+  # write patch summary data
+  if(success == TRUE){
+    fwrite(patch_summary, file = "data/watlas_2018/data_patch_summaries.csv",
+      append = TRUE)
+  }
   
   
   # prepare for telemetry
   {
-    test <- data
-    # convert to lat-long
-    coords <- test %>% 
-      st_as_sf(coords = c("x", "y")) %>% 
-      `st_crs<-`(32631) %>% 
-      st_transform(4326) %>% 
-      st_coordinates()
+    data_for_ctmm <- setDT(patch_points)[,.(id, tide_number, x, y, patch, time, VARX, VARY)]
     
-    names(coords) <- c("location.long","location.lat")
+    # aggregate within a patch to 10 seconds
+    data_for_ctmm <- split(data_for_ctmm, f = data_for_ctmm$patch) %>% 
+      map(wat_agg_data, interval = 60) %>% 
+      bind_rows()
     
-    test[,HDOP:=sqrt(VARX+VARY)]
-    test <- test[,.(id, ts, HDOP)]
-    test[,ts:=fastPOSIXct(ts)]
-    setnames(test, c("individual.local.identifier", "timestamp", "HDOP"))
-    test[,`:=`(location.long = coords[,1], location.lat = coords[,2])]
+    # make each patch an indiv
+    setDT(data_for_ctmm)
+    data_for_ctmm[,individual.local.identifier:= paste(id, tide_number, patch,
+                                                       sep = "_")]
+    # get horizontal error
+    data_for_ctmm[,HDOP := sqrt(VARX+VARY)/10]
+    # subset columns
+    data_for_ctmm <- data_for_ctmm[,.(individual.local.identifier, time, x, y, HDOP)]
+
+    # get new names
+    setnames(data_for_ctmm, old = c("x", "y", "time"), 
+      new = c("UTM.x","UTM.y", "timestamp"))
+    
+    # convert time to posixct
+    data_for_ctmm[,timestamp:=as.POSIXct(timestamp, origin = "1970-01-01")]
+    # add UTM zone
+    data_for_ctmm[,zone:="31 +north"]
+    
   }
   
   # make telemetry
   {
-    tel <- as.telemetry(test)
+    tel <- as.telemetry(data_for_ctmm)
   }
   
-  # ctmm
+  # ctmm section
   {
-    outliers <- outlie(tel)
-    q90 <- quantile(outliers[[1]], probs = c(0.99))
+    # get the outliers but do not plot
+    outliers <- map(tel, outlie, plot=FALSE)
+    # get a list of 99 th percentile outliers
+    q90 <- map(outliers, function(this_outlier_set){
+      quantile(this_outlier_set[[1]], probs = c(0.99))
+    })
+    # remove outliers from telemetry data
+    tel <- pmap(list(tel, outliers, q90), 
+                function(this_tel_obj, this_outlier_set, outlier_quantile) 
+                  {this_tel_obj[-(which(this_outlier_set[[1]] >= outlier_quantile)),]})
     
-    tel <- tel[-(which(outliers[[1]] >= q90)),]
+    # some patches have no data remaining, filter them out
+    tel <- keep(tel, function(this_tel){nrow(this_tel) > 0})
     
-    # make variogram
-    vg <- variogram(tel)
+    # guess ctmm params
+    guess_list <- lapply(tel, ctmm.guess, interactive = F)
     
-    mod <- ctmm.fit(tel)
+    # run ctmm fit
+    mod <- map2(tel, guess_list, function(obj_tel, obj_guess){
+      ctmm.fit(obj_tel, CTMM = obj_guess)
+    })
   }
   
   message("model fit!")
