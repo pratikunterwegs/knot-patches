cc6add3c49e34dba2a44c4149f7c63d1b396b7a4
pratik <pratikgupte16@gmail.com>
2020-05-30 10:19:40 +0200

    use ncls for overlap

--- code/code_03_overlap_ncls.py
+++ code/code_03_overlap_ncls.py
@@ -13,33 +13,45 @@ data.head()
 # assign unique patch id
 data['uid'] = np.arange(0, data.shape[0])
 
+# overwrite data with uid
+data.to_csv("data/data2018/data_2018_patch_summary.csv",
+            index=False)
+
 # make the ncls
 ncls = NCLS(np.asarray(data.time_start),
             np.asarray(data.time_end),
             np.asarray(data.uid))
 
-# the python way
-it = ncls.find_overlap(data.iloc[1].time_start,
-                       data.iloc[1].time_end)
-
-for i in it:
-    print(i)
-
 # look at all the overlaps in time
 # get a dataframe of the overlapping pairs and the extent of overlap
 data_list = []
-for i in np.arange():
+for i in np.arange(len(data)):
+    ncls = NCLS(np.asarray(data[i:].time_start),
+                np.asarray(data[i:].time_end),
+                np.asarray(data[i:].uid))
     it = ncls.find_overlap(data.iloc[i].time_start,
                            data.iloc[i].time_end)
     # get the unique patch ids overlapping
     overlap_id = []
     overlap_extent = []
+    # get the extent of overlap
     for x in it:
         overlap_id.append(x[2])
         overlap_extent.append(min(x[1], data.iloc[i].time_end) - max(x[0], data.iloc[i].time_start))
+    # add the overlap id for each obs
     uid = [i] * len(overlap_id)
-    tmp_data = pd.DataFrame(list(zip(uid, overlap_id, overlap_extent)),
-                            columns=['uid', 'overlap_id', 'overlap_extent'])
-    tmp_data = tmp_data[tmp_data.uid != tmp_data.overlap_id]
-    data_list.append(tmp_data)
+    # zip the tuples together
+    tmp_data = list(zip(uid, overlap_id, overlap_extent))
+    # convert to lists
+    tmp_data = list(map(list, tmp_data))
+    tmp_data = list(filter(lambda x: x[0] != x[1], tmp_data))
+    # tmp_data = tmp_data[tmp_data.uid != tmp_data.overlap_id]
+    data_list = data_list + tmp_data
+
+# concatenate to dataframe
+data_overlap = pd.DataFrame(data_list,
+                         columns=['uid', 'overlap_id', 'overlap_extent'])
+
+# save data
+data_overlap.to_csv("data/data2018/data_time_overlaps_patches_2018.csv", index=False)
 # wip
