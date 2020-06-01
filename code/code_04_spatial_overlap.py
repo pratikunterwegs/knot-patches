3e964645b0396cd268bf500a912a4b46f0abd3bc
P.R. Gupte <P284074@Workspace.rug.nl>
2020-06-01 21:45:02 +0200

    try regular geopandas instead

--- code/code_04_spatial_overlap.py
+++ code/code_04_spatial_overlap.py
@@ -12,13 +12,26 @@ from shapely.geometry import Point, MultiPoint, LineString, MultiLineString, Pol
 # import ckdtree
 from scipy.spatial import cKDTree
 
-# read in data
+# import helper functions
+from helper_functions import simplify_geom, ckd_distance
+
+# read in spatial data
 patches = gpd.read_file("data/data2018/spatials/patches_2018.gpkg")
 patches.head()
 patches.crs = {'init': 'epsg:32631'}
-# reproject
-unique_locs.crs = {'init' :'epsg:4326'}
 
-# reproject spatials to 43n epsg 32643
+# read in temporal overlaps
+patch_overlaps = pd.read_csv("data/data2018/data_time_overlaps_patches_2018.csv")
 
-roads = roads.to_crs({'init': 'epsg:32643'})
+# for each overlap uid/overlap_id get the ckd distance of
+# the corresponding rows in the spatial
+spatial_cross = []
+for i in np.arange(len(patch_overlaps)):
+    # get the geometries
+    g_a = patches.iloc[patch_overlaps.iloc[i].uid]
+    g_b = patches.iloc[patch_overlaps.iloc[i].overlap_id]
+    covers = g_a.geometry.intersects(g_b.geometry)
+    spatial_cross.append(covers)
+
+# convert to series and add to data frame
+patch_overlaps['spatial_overlap'] = pd.Series(spatial_cross)
