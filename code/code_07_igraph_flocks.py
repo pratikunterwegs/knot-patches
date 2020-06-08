e0d40fccbebfd2b589cdceeb4837482c46d17e78
P.R. Gupte <P284074@Workspace.rug.nl>
2020-06-08 17:28:07 +0200

    simple network no weights

--- code/code_07_igraph_flocks.py
+++ code/code_07_igraph_flocks.py
@@ -16,17 +16,11 @@ g = ig.Graph.TupleList(data.values,
                        weights=True, directed=False)
 
 # find communities, this is a two step process
-g_time_comm = g.community_fastgreedy(weights=data['temporal_overlap_seconds'])
-time_clusters = g_time_comm.as_clustering()
-
-## NETWORKS FOR SPACE OVERLAP
-# find communities, this is a two step process
-g_area_comm = g.community_fastgreedy(weights=data['spatial_overlap_area'])
-area_clusters = g_area_comm.as_clustering()
+g_comm = g.community_fastgreedy()
+clusters = g_comm.as_clustering()
 
 # assign cluster
-data['time_cluster'] = pd.Series(time_clusters.membership)
-data['area_clusetr'] = pd.Series(area_clusters.membership)
+data['flock'] = pd.Series(clusters.membership)
 
 # export
 data.to_csv("data/data2018/data_patch_flocks_2018.csv")
