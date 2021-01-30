e992b1a5c48296870dc2a4821c21ef6ffb2a20d2
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-30 21:05:59 +0100

    update render code

--- render_books.sh
+++ render_books.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# style rmd
+Rscript --vanilla --slave -e 'styler::style_dir(".", filetype = "Rmd", recursive = FALSE)'
+
 Rscript --slave -e 'bookdown::render_book("index.rmd")'
-Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_book"); warnings()'
+
+Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2")'
+
+# to render source files
+Rscript --slave -e 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
